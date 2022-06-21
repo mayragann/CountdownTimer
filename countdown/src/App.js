c95279d5029904b37dbacd0a12d7729151e6efb2
mayragann <mayragann@gmail.com>
2022-06-21 16:13:02 -0400

    Started to add functionality to countdown timer

--- countdown/src/App.js
+++ countdown/src/App.js
@@ -1,23 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 id="head"> React Countdown</h1>
-      </header>
-      <div id="Countdown">
-        <ul>
-          <li><span>00</span>Days</li>
-          <li><span>00</span>Hours</li>
-          <li><span>00</span>Days</li>
-          <li><span>00</span>Seconds</li>
-        </ul>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount(){
+    this.startInterval()
+  }
+  startInterval = () => {
+    const second = 1000, minute = second * 60, hour = minute * 60, day = hour* 24
+    let x = setInterval(
+      function (){
+       document.getElementById("days").innerText = "00"
+       document.getElementById("hours").innerText = "00"
+       document.getElementById("minutes").innerText = "00"
+       document.getElementById("seconds").innerText = "00"
+      }, second)
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 id="head"> React Countdown</h1>
+        </header>
+        <div id="Countdown">
+          <ul>
+            <li><span id="days"></span>Days</li>
+            <li><span id="hours"></span>Hours</li>
+            <li><span id="minutes"></span>Minutes</li>
+            <li><span id="seconds"></span>Seconds</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  }
+
 }
 
 export default App;

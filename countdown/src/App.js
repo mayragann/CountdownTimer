f6732432ac4904424892972dcd644ca554a20164
mayragann <mayragann@gmail.com>
2022-06-29 15:19:41 -0400

    Added Bootstrap + Ability To toggle between all

--- countdown/src/App.js
+++ countdown/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MyTimer from './Components/Timer/Timer';
 import MyStopwatch from './Components/Timer/Stopwatch/Stopwatch';
@@ -7,13 +7,24 @@ import MyTime from './Components/Timer/CurrentTime/CurrentTime'
 function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600);
+  const [toggleTimer, setToggleTimer] = useState(true)
+  const [toggleWatch, setToggleWatch] = useState(true)
+  const [toggleCurrent, setToggleCurrent] = useState(true)
 
   return (
     <div>
+      <button onClick={() => setToggleTimer(!toggleTimer)} className="btn-btn-primary mb-5">My Timer</button>
+      {toggleTimer && (
       <MyTimer expiryTimestamp={time} />
-
+      )}
+      <button onClick={() => setToggleWatch(!toggleWatch)} className="btn-btn-primary mb-5">My Stopwatch</button>
+      {toggleWatch && (
       <MyStopwatch />
+      )}
+      <button onClick={() => setToggleCurrent(!toggleCurrent)} className="btn-btn-primary mb-5">My Current Time</button>
+      {toggleCurrent && (
       <MyTime />
+      )}
     </div>
   );
 }
